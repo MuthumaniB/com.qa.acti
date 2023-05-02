ea52c99664817b32aa3b888bc202912b4c8cf91b
manit <manit@DESKTOP-4ICBJKC.lan>
2023-05-02 16:42:44 -0500

    added author details to enter page

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -1,7 +1,14 @@
 package com.acti.pages;
 
 import com.acti.base.DriverScript;
+/*
+ * Name : EnterPage
+ * Description : Manages the locators and methods of enter page 
+ * Developed By :Mani
+ * Reviewed By : Santhosh
+ * Date Reviewed : 04-27-2023
+ */
 
 public class EnterPage extends DriverScript{
-
+	
 }
