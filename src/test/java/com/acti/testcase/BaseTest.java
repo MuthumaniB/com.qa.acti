14570098f4e1d637b8d5dee08717f19ebe50556c
manit <manit@DESKTOP-4ICBJKC.lan>
2023-05-03 00:07:38 -0500

    added taskpage

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -6,16 +6,19 @@ import org.testng.annotations.BeforeMethod;
 import com.acti.base.DriverScript;
 import com.acti.pages.EnterPage;
 import com.acti.pages.LoginPage;
+import com.acti.pages.TaskPage;
 
 public class BaseTest extends DriverScript {
 	LoginPage lp;
 	EnterPage ep;
+	TaskPage tp;
 	@BeforeMethod
 	public void pretest()
 	{
 		initApplication();
 		 lp = new LoginPage();
 		 ep=new EnterPage();
+		 tp=new TaskPage();
 	}
 	@AfterMethod
 	public void teardown()
